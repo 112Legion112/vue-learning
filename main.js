1dcaf902f2be24fbadfe34ab9b7a92f77a98db10
Max <ageev.maks2014@gmail.com>
2018-06-17 15:04:32 +0500

    2. Attribute Binding

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var  app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: 'A pair of warm, fuzzy socks',
+    image: './assets/Socks.jpg',
+    altText: 'A pair of socks',
+    link: 'https://twitter.com/Max77385676',
   }
 });
